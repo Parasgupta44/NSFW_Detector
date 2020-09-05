9e18ab3467f28c526918f97e7d4eaeb7a401e519
unknown <paras.gupta986745@gmail.com>
2020-09-05 16:03:02 +0530

    Adding gitignore

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Deployment-Deep-Learning-Model
+# NSFW detector using Convolution Neural Networks
+
+The given repository builds a decent model for NSFW detection using [NudeNetDataset](https://archive.org/details/NudeNet_classifier_dataset_v1) with the help of [Keras](https://keras.io/). Convolution Neural Networks are used for training the dataset. The model has been trained on a section of the dataset on local machines for the ease of deployment.
+
+![alt text](./figs/Box.gif)
+
+
+## Dataset
+
+
+[NudeNetDataset](https://archive.org/details/NudeNet_classifier_dataset_v1) is used for training the model (a section of it). It comprises of three classes - nude (raw), sexy (can be passed further) and safe.
+
+
+
+## Pre-trained Models
+
+The Following models were used for training the dataset. Also, image size was altered to check the dependencies.
+- [VGG16](https://arxiv.org/abs/1409.1556)
+- [InceptionV3](https://arxiv.org/abs/1512.00567)
+
+VGG16 is used in the deployed model.
+
+
+## Training and Testing
+
+
+### Training
+
+Make sure to have the requirements installed as provided in the `requirements.txt`.
+
+```bash
+pip install -r requirements.txt
+```
+
+Use the notebook provided in `models/train.ipynb` file in your preferred python execution setup.
+
+The best model outputs will be automatically saved with the code provided in the file itself,.
+
+### Testing
+
+The code provided in the [`app.py`](./app.py) file is used for testing your image and rendering the main page.
+
+
+## Deployment
+
+### Flask
+
+The model had been deployed using [Flask](https://flask.palletsprojects.com/) web Framework. To run the model on localhost run:
+
+```python
+python app.py 
+
+```
+### Usage
+
+After spinning the server up online, just upload your image to get the predicted result.
--- app.py
+++ app.py
@@ -1,38 +1,29 @@
 from __future__ import division, print_function
 # coding=utf-8
-import sys
-import os
 import glob
-import re
-import numpy as np
 
-# Keras
+# Keras and Tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from tensorflow.keras.applications.vgg16 import VGG16
-import cv2
-import math
 import os
 from glob import glob
-from scipy import stats as s
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-# from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
 
-
 # Load the base model and weights
 base_model = VGG16(weights='imagenet', include_top=False, input_shape=(80, 80, 3))
-#defining the model architecture
+
+# defining the model architecture
 model = Sequential()
 model.add(Dense(1024, activation='relu', input_shape=(2048,)))
 model.add(Dropout(0.5))
@@ -42,26 +33,14 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
+# 3 classes - nude, safe and sexy (naming convention followed from dataset)
 model.add(Dense(3, activation='softmax'))
+
 # loading the trained weights
 model.load_weights("models/weight_80_36000.hdf5")
+
 # compiling the model
-model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
-
-# Model saved with Keras model.save()
-# MODEL_PATH = 'models/weight_80_36000.hdf5'
-
-# Load your trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
 
 def predict_if_safe(path):
@@ -93,16 +72,23 @@ def predict_if_safe(path):
     except:
         res = "Please upload a valid file"
 
+    # remove the file after processing
     files = glob('uploads\*')
     for f in files:
         os.remove(f)
+    if str(res) == "nude":
+        res = "Contains strong adult content."
+    elif str(res) == 'sexy':
+        res = "Contains adult content. May be inappropriate."
+    elif str(res) == 'safe':
+        res = "Safe for usage."
 
     return str(res)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
+    # Our home page (only one !!)
     return render_template('index.html')
 
 
@@ -126,4 +112,3 @@ def upload():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
